a5bee3da13bd58a8b3141f9f64755e368bdf3abf
YANG <yang@YANGdeMacBook-Pro.local>
2017-07-19 22:01:28 +0800

    Initial Commit

---
+++ GradientNavigationBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GradientNavigationBar
+//
+//  Created by YANG on 2017/7/19.
+//  Copyright © 2017年 洪海 杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GradientNavigationBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GradientNavigationBar
+//
+//  Created by YANG on 2017/7/19.
+//  Copyright © 2017年 洪海 杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
